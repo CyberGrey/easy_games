5c34526b5b7815e3e458a8e738a59adc4982e62d
Sergey Melnik <ic-energo@ya.ru>
2024-03-16 16:31:16 +0800

    edit board

--- tikTokToe.py
+++ tikTokToe.py
@@ -13,11 +13,13 @@ win_comb = {'1': {'1-1', '1-2', '1-3'},
             }
 
 def print_board(board):
-    print(board['1-1'] + '|' + board['1-2'] + '|' + board['1-3'])
-    print('---+---+---')
-    print(board['2-1'] + '|' + board['2-2'] + '|' + board['2-3'])
-    print('---+---+---')
-    print(board['3-1'] + '|' + board['3-2'] + '|' + board['3-3'])
+    print(' ----------- ')
+    print('|' + board['1-1'] + '|' + board['1-2'] + '|' + board['1-3'] + '|')
+    print(' ---+---+--- ')
+    print('|' + board['2-1'] + '|' + board['2-2'] + '|' + board['2-3'] + '|')
+    print(' ---+---+--- ')
+    print('|' + board['3-1'] + '|' + board['3-2'] + '|' + board['3-3'] + '|')
+    print(' ----------- ')
     
 def win_player(board, turn):
     player_set = set()
